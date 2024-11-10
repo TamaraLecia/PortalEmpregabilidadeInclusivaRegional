d73472e75f70a45cbf54146f01830251bbbfad1c
Tamara Lécia <139869944+TamaraLecia@users.noreply.github.com>
2024-11-10 11:17:34 -0300

    Adicionando Spring Boot

---
+++ PortalEmpregabilidadeInclusivaRegional/peir/src/main/java/com/peir/peir/PeirApplication.java
@@ -0,0 +1,13 @@
+package com.peir.peir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeirApplication.class, args);
+	}
+
+}
