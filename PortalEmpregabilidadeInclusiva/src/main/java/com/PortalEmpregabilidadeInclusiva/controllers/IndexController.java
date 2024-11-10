4ee3e62a907f818057875474551a64544ff2cdef
Tamara Lécia <139869944+TamaraLecia@users.noreply.github.com>
2024-11-10 12:16:47 -0300

    Organização das pastas em MVC

---
+++ PortalEmpregabilidadeInclusiva/src/main/java/com/PortalEmpregabilidadeInclusiva/controllers/IndexController.java
@@ -0,0 +1,5 @@
+package com.PortalEmpregabilidadeInclusiva.controllers;
+
+public class IndexController {
+
+}
---
+++ PortalEmpregabilidadeInclusiva/src/test/java/com/PortalEmpregabilidadeInclusiva/PortalEmpregabilidadeInclusiva/PortalEmpregabilidadeInclusivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortalEmpregabilidadeInclusiva.PortalEmpregabilidadeInclusiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalEmpregabilidadeInclusivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
