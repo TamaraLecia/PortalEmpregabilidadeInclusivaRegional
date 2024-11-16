1c770d3bbc5100673eff0961bf29940e60fc1bbc
Thierre999 <thierrepib@gmail.com>
2024-11-16 09:49:19 -0300

    Conexão com xampp

--- ProjetoPEI/pei/src/main/java/com/pei/dao/DataBaseConfiguracao.java
+++ ProjetoPEI/pei/src/main/java/com/pei/dao/DataBaseConfiguracao.java
@@ -1,5 +1,20 @@
 package com.pei.dao;
 
-public class DataBaseConfiguracao {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DataBaseConfiguracao {
+    public static Connection conectar() {
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/pei_database"; 
+            String usuario = "root"; 
+            String senha = ""; 
+
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
