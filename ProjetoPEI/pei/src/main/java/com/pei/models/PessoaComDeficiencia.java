e520441900658823d141282267b7a0bc0dd2aa8e
Thierre999 <thierrepib@gmail.com>
2024-11-16 11:16:01 -0300

    Métodos cadastrar, fazerLogin, fazerLogout em pessoaComDeficienciaDAO

--- ProjetoPEI/pei/src/main/java/com/pei/models/PessoaComDeficiencia.java
+++ ProjetoPEI/pei/src/main/java/com/pei/models/PessoaComDeficiencia.java
@@ -3,7 +3,7 @@ package com.pei.models;
 import lombok.Data;
 
 @Data
-public class PessoaComDeficiencia {
+public class PessoaComDeficiencia extends Pessoa{
 	private String deficiencia;
 	private String formacao;
 	private String experiencia;
