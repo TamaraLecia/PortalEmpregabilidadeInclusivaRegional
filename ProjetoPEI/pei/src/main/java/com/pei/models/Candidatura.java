44241ac3e12b1cb88981ba6968923a3c2ac56803
Thierre999 <thierrepib@gmail.com>
2024-11-16 16:18:39 -0300

    Criação das classes Empresa, Candidatura, CandidaturaDAO e CandidaturaControler

---
+++ ProjetoPEI/pei/src/main/java/com/pei/models/Candidatura.java
@@ -0,0 +1,11 @@
+package com.pei.models;
+
+import lombok.Data;
+
+@Data
+public class Candidatura {
+    private Integer id;
+    private String nome;
+    private int idVaga;
+    private String titulo;
+}
---
+++ ProjetoPEI/pei/src/main/java/com/pei/models/Empresa.java
@@ -0,0 +1,10 @@
+package com.pei.models;
+
+import lombok.Data;
+
+@Data
+public class Empresa {
+   private String cnpj;
+   private String razaoSocial;
+   private String vagas; 
+}
