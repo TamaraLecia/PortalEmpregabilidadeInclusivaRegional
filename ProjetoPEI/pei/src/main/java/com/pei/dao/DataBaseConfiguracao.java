bc65eb03ecb3cf68335c3367aa6d4047ce89c9da
Thierre999 <165316138+Thierre999@users.noreply.github.com>
2024-11-16 09:39:52 -0300

    Update DataBaseConfiguracao.java

--- ProjetoPEI/pei/src/main/java/com/pei/dao/DataBaseConfiguracao.java
+++ ProjetoPEI/pei/src/main/java/com/pei/dao/DataBaseConfiguracao.java
@@ -1,20 +1,6 @@
 package com.pei.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class DataBaseConfiguracao {
-  public static Connection conectar() {
-        try {
 
-            String url = "jdbc:mysql://localhost:3306/pei_database"; 
-            String usuario = "root"; 
-            String senha = ""; 
-
-            return DriverManager.getConnection(url, usuario, senha);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
