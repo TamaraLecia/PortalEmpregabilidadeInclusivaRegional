6dabdc7c1b1697aa54e4f31b530afc857b73bb05
TamaraLecia <tamaralecia976@gmail.com>
2024-11-20 06:37:04 -0300

    Criação dos métodos logout e do método buscar vaga na classe PessoaComDeficiencia

--- ProjetoPEI/pei/src/main/java/com/pei/controller/PessoaComDeficienciaController.java
+++ ProjetoPEI/pei/src/main/java/com/pei/controller/PessoaComDeficienciaController.java
@@ -2,7 +2,13 @@ package com.pei.controller;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pei.dao.PessoaComDeficienciaDAO;
 import com.pei.models.PessoaComDeficiencia;
+import com.pei.models.Vaga;
 
 @RestController
 @RequestMapping("/pessoas-com-deficiencia")
@@ -51,6 +58,49 @@ public class PessoaComDeficienciaController {
             }
         } catch (SQLException e) {
             return "Erro: " + e.getMessage();
+        }
+    }
+
+        //Endpoint para realizar o logout
+    @WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+    public class Logout extends HttpServlet {
+        public void  realizarLougout(HttpServletRequest requisicao, HttpServletResponse resposta) {
+            try {
+                HttpSession sessao = requisicao.getSession(); //pegando referencia da sessão para realizar logout
+                sessao.invalidate(); //apagando a sessão do usuario
+
+                //Leva o usuario para a pagina inicial
+                RequestDispatcher redireciona = requisicao.getRequestDispatcher("index.html");
+                redireciona.forward(requisicao, resposta);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    //Endpoint para buscar vaga
+    @PostMapping("/bucarVaga")
+    public void buscarVaga(@RequestParam String titulo, String localizacao){
+        try {
+            List<Vaga> vagas = pessoaComDeficienciaDAO.bucarVaga(titulo, localizacao);
+            if(vagas != null){
+                for(Vaga vaga : pessoaComDeficienciaDAO.bucarVaga(titulo, localizacao)){
+                    System.out.println("Titulo: " + vaga.getTitulo());
+                    System.out.println("Requisitos: " + vaga.getRequisitos());
+                    System.out.println("Localização: " + vaga.getLocalizacao());
+                    System.out.println("Acessibilidade: " + vaga.getAcessibilidade());
+                    System.out.println("Empresa: " + vaga.getEmpresa());
+                    System.out.println("Beneficios: " + vaga.getBeneficios());
+                    System.out.println("Salário: " + vaga.getSalario());
+                    System.out.println();
+                    System.out.println();
+                    System.out.println();
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Vaga não encontrada");
         }
     }
 }
--- ProjetoPEI/pei/src/main/java/com/pei/dao/PessoaComDeficienciaDAO.java
+++ ProjetoPEI/pei/src/main/java/com/pei/dao/PessoaComDeficienciaDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.pei.models.PessoaComDeficiencia;
+import com.pei.models.Vaga;
 
 public class PessoaComDeficienciaDAO {
     private Connection connection;
@@ -59,6 +62,58 @@ public class PessoaComDeficienciaDAO {
             }
         }
         return null; // Retorna null se as credenciais estiverem erradas
+    }
+
+        //Metodo para buscar vaga
+    public List<Vaga> bucarVaga(String titulo, String localizacao){
+        String sql = "SELECT * FROM pessoa_com_deficiencia WHERE titulo = ? || localizacao = ? ";
+        List<Vaga> vagas = new ArrayList<Vaga>();
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        //recupera os dados do banco
+        ResultSet rset = null;
+
+        try {
+            con = DataBaseConfiguracao.conectar();
+            
+            pstm = (PreparedStatement) con.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+                Vaga vaga = new Vaga();
+
+                vaga.setIdVaga(rset.getInt("id"));
+                vaga.setTitulo(rset.getString("titulo"));
+                vaga.setRequisitos(rset.getString("requisitos"));
+                vaga.setSalario(rset.getDouble("salario"));
+                vaga.setLocalizacao(rset.getString("localizacao"));
+                vaga.setAcessibilidade(rset.getString("acessibilidade"));
+                vaga.setEmpresa(rset.getString("empresa"));
+                vaga.setBeneficios(rset.getString("beneficios"));
+
+                vagas.add(vaga);
+                return vagas;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(rset != null){
+                    rset.close();
+                }
+                if(pstm != null){
+                    pstm.close();
+                }
+                if(con != null ){
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return bucarVaga(titulo, localizacao);
     }
 
    
