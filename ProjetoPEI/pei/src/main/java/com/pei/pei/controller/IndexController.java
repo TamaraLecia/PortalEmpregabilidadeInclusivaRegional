439294699d9c53b79bb0885c0c8265a2c2499d01
Tamara Lécia <139869944+TamaraLecia@users.noreply.github.com>
2025-08-01 10:11:28 -0300

    Delete ProjetoPEI directory

--- ProjetoPEI/pei/src/main/java/com/pei/pei/controller/IndexController.java
+++
@@ -1,5 +0,0 @@
-package com.pei.pei.controller;
-
-public class IndexController {
-
-}
--- ProjetoPEI/peiFrontend/js/TelaInicio.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.menu-header').forEach(header => {
-    header.addEventListener('click', () => {
-      header.classList.toggle('active');
-    });
-  });
-  
